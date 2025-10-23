5c5536ff016d8ae3db43cfb9296b05246fc12ebf
Usmanblessing043 <usmanblesssing043@gmail.com>
2025-10-23 11:59:37 +0100

    'backhendupdate'

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,6 +4,8 @@ env
 const {uservideomodel} = require("../model/user.model")
 uservideomodel
 const { v4: uuidv4 } = require("uuid");
+const {Resend} = require ("resend")
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 
 const bcrypt = require('bcryptjs')
@@ -144,6 +146,48 @@ const Createroom =  (req, res) => {
 
 }
 
+// const ForgetPassword = async (req, res) => {
+//   try {
+//     const { email } = req.body;
+//     console.log(email);
+
+//     const user = await uservideomodel.findOne({ email });
+//     if (!user) {
+//       return res.status(404).send({ message: "User not found", status: false });
+//     }
+
+//     const resetToken = crypto.randomBytes(32).toString("hex");
+//     user.resetToken = resetToken;
+//     user.resetTokenExpire = Date.now() + 10 * 60 * 1000; // 10 minutes
+//     await user.save();
+
+//     const resetLink = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
+
+//     const transporter = nodemailer.createTransport({
+//       service: "gmail",
+//       auth: {
+//         user: process.env.EMAIL_USER,
+//         pass: process.env.EMAIL_PASS,
+//       },
+//     });
+
+//     await transporter.sendMail({
+//       from: `"Video Conference" <${process.env.EMAIL_USER}>`,
+//       to: email,
+//       subject: "Password Reset Link",
+//       html: `
+//         <p>You requested a password reset.</p>
+//         <p>Click <a href="${resetLink}">here</a> to reset your password.</p>
+//         <p>This link will expire in 10 minutes.</p>
+//       `,
+//     });
+
+//     res.status(200).send({ message: "Reset link sent to your email", status: true });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(500).send({ message: error.message, status: false });
+//   }
+// };
 const ForgetPassword = async (req, res) => {
   try {
     const { email } = req.body;
@@ -154,37 +198,28 @@ const ForgetPassword = async (req, res) => {
 
     const resetToken = crypto.randomBytes(32).toString("hex");
     user.resetToken = resetToken;
-    user.resetTokenExpire = Date.now() + 10 * 60 * 1000; // 10 minutes
+    user.resetTokenExpire = Date.now() + 10 * 60 * 1000; // 10 mins
     await user.save();
 
     const resetLink = `${process.env.FRONTEND_URL}/resetpassword/${resetToken}`;
 
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    await transporter.sendMail({
-      from: `"Video Conference" <${process.env.EMAIL_USER}>`,
+    await resend.emails.send({
+      from: 'Video Conference <usmanblessing043@gmail.com>',
       to: email,
-      subject: "Password Reset Link",
+      subject: 'Password Reset Link',
       html: `
         <p>You requested a password reset.</p>
         <p>Click <a href="${resetLink}">here</a> to reset your password.</p>
-        <p>This link will expire in 10 minutes.</p>
+        <p>This link expires in 10 minutes.</p>
       `,
     });
 
     res.status(200).send({ message: "Reset link sent to your email", status: true });
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).send({ message: error.message, status: false });
   }
 };
-
 
 // =============== RESET PASSWORD ===============
 const Resetpassword = async (req, res) => {
